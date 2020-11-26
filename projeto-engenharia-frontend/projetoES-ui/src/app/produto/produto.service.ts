42913043a018e52d75777c3722a11f89982321f3
Kanilds <pedro_h.o@hotmail.com>
2020-11-26 01:51:01 -0300

    service Produto BUGFIX

--- projeto-engenharia-frontend/projetoES-ui/src/app/produto/produto.service.ts
+++ projeto-engenharia-frontend/projetoES-ui/src/app/produto/produto.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class ProdutoService {
 
-  constructor(private http: HttpClientModule) { }
+  produtoUrl = 'http://localhost:8080/rest/produto';
+
+  constructor(private http: HttpClient) { }
+
+  pesquisar(): Promise<any> {
+    return this.http.get(`${this.produtoUrl}`)
+      .toPromise()
+      .then(response => {
+        console.log(response);
+      })
+  }
 }
