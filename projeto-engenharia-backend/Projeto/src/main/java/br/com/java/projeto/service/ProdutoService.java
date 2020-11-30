776267854087a104eaffdc23ac92df0fe67a6be2
Leandro J <leandrojoapi@gmail.com>
2020-11-29 23:39:59 -0300

    Filtro CORS

--- projeto-engenharia-backend/Projeto/src/main/java/br/com/java/projeto/service/ProdutoService.java
+++ projeto-engenharia-backend/Projeto/src/main/java/br/com/java/projeto/service/ProdutoService.java
@@ -5,6 +5,8 @@ import br.com.java.projeto.domain.Produto;
 import com.google.gson.Gson;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("produto")
